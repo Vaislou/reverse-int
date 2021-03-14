a6ad8e4a26e0dab860966d2e9e5e08894c971331
Vaislou <vaislou@gmail.com>
2021-03-14 19:10:58 +0300

    fix: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -(n);
+    }
+  let number = n.toString().split('').reverse().join('');
+  return result = Number(number);
 }
